6d11219eebea11fadb8d1d8325d62ea94e4b5464
Moonkey <moonkey48@icloud.com>
2023-09-09 15:12:31 +0900

    [setting] 기본 프로젝트 생성

---
+++ RetrospectDiary/RetrospectDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RetrospectDiary
+//
+//  Created by Seungui Moon on 2023/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
